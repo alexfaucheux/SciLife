6c40d1b422b74dea873fc5625174954f66eaca29
JoshRomero <38444415+JoshRomero@users.noreply.github.com>
2018-11-02 14:01:51 -0500

    Update Test/TestDialog.java

--- Test/TestDialog.java
+++ Test/TestDialog.java
@@ -1,4 +1,5 @@
 package test;
+
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -6,20 +7,24 @@ import javax.swing.JTextField;
 
 public class TestDialog {
 
-    public static void main(String[] args) {
-        Object[] options1 = { "Try This Number", "Choose A Random Number",
-                "Quit" };
+	public static void main(String[] args) {
+		Object[] options1 = { "Try This Number", "Choose A Random Number", "Quit" };
 
-        JPanel panel = new JPanel();
-        panel.add(new JLabel("Enter number between 0 and 1000"));
-        JTextField textField = new JTextField(10);
-        panel.add(textField);
+		JPanel panel = new JPanel();
+		panel.add(new JLabel("Enter number between 0 and 1000"));
+		JTextField textField = new JTextField(10);
+		panel.add(textField);
 
-        int result = JOptionPane.showOptionDialog(null, panel, "Enter a Number",
-                JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE,
-                null, options1, null);
-        if (result == JOptionPane.YES_OPTION){
-            JOptionPane.showMessageDialog(null, textField.getText());
-        }
-    }
+		int result = JOptionPane.showOptionDialog(null, panel, "Enter a Number", JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.PLAIN_MESSAGE, null, options1, null);
+		if (result == JOptionPane.YES_OPTION) {
+			JOptionPane.showMessageDialog(null, textField.getText());
+
+			UIManager UI = new UIManager();
+			UIManager.put("OptionPane.background", new ColorUIResource(255, 0, 0));
+			UIManager.put("Panel.background", new ColorUIResource(255, 0, 0));
+			JOptionPane.showMessageDialog(null, "Text", "SetColor", JOptionPane.INFORMATION_MESSAGE);
+
+		}
+	}
 }
